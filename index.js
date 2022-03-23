a8613b6064949faff36b12053857e1e85429c1a1
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-03-22 20:27:29 -0700

    Updated methods to chained

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ class User {
     }
     makeDeposit(amount){
         this.accountBalance = this.accountBalance + amount;
+        return this
     }
     makeWithdrawl(amount){
         this.accountBalance = this.accountBalance - amount;
+        return this
     }
     displayBalance(){
         console.log("User: " + this.name + ", account balance is: $" + this.accountBalance);
+        return this
     }
 
     transferMoney(otherUser, amount){
         this.accountBalance = this.accountBalance - amount;
         otherUser.accountBalance = otherUser.accountBalance + amount;
+        return this
     }
 }
 
@@ -24,24 +28,8 @@ const thomas = new User("Thomas", "thomas@gmail.com");
 const donnie = new User("Donovan", "donny@gmail.com");
 const jayden = new User("Jayden", "jayden@gmail.com");
 
-thomas.makeDeposit(300);
-thomas.makeDeposit(300);
-thomas.makeDeposit(200);
-thomas.makeWithdrawl(375);
-thomas.displayBalance();
-
-donnie.makeDeposit(300);
-donnie.makeDeposit(250);
-donnie.makeWithdrawl(75);
-donnie.makeWithdrawl(275);
-donnie.displayBalance();
-
-jayden.makeDeposit(650);
-jayden.makeWithdrawl(50);
-jayden.makeWithdrawl(500);
-jayden.makeWithdrawl(50);
-jayden.displayBalance();
-
-thomas.transferMoney(jayden, 200);
-thomas.displayBalance();
+thomas.makeDeposit(300).makeDeposit(300).makeDeposit(200).makeWithdrawl(375).displayBalance();
+donnie.makeDeposit(300).makeDeposit(250).makeWithdrawl(75).makeWithdrawl(275).displayBalance();
+jayden.makeDeposit(650).makeWithdrawl(50).makeWithdrawl(500).makeWithdrawl(50).displayBalance();
+thomas.transferMoney(jayden, 200).displayBalance();
 jayden.displayBalance();
